2dfb63457600d3b82e0d2acb389bfabbb6cff0a6
karishma patel <kpatel44@twu.edu>
2026-09-16 19:19:15 -0500

    created assignment 4 files

---
+++ Assignments/Assignments_4/q3.py
@@ -0,0 +1,10 @@
+# question 3
+subject1 = 89
+subject2 = 90
+subject3 = 83
+
+total = subject1 + subject2 + subject3
+average = total / 3
+
+print(f"Total: {total} out of 300")
+print(f"Average: {average:.2f}%")
